d2eb9ca562ea61c16dcf0ab0c3de0dd2b1525aeb
elkafrawyel <mahmoud.ashraf.elkafrawy@gmai.com>
2019-10-25 19:54:51 +0200

    set network text methods and remove style

--- frameView/src/main/java/com/elkafrawyel/FrameView.kt
+++ frameView/src/main/java/com/elkafrawyel/FrameView.kt
@@ -52,7 +52,6 @@ public class FrameView : FrameLayout {
         internetView = inflater.inflate(R.layout.custom_view_network, null)
         networkText = internetView.findViewById(R.id.networkText)
 
-
         emptyView = inflater.inflate(R.layout.custom_view_empty, null)
         emptyText = emptyView.findViewById(R.id.emptyText)
 
